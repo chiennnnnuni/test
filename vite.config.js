c117be68cb3031d2c33498d6c47c6404a593af8d
LHchien33 <judy263562@gmail.com>
2024-12-10 15:32:20 +0800

    .

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig(({ mode }) => {
   return {
-    base: '/test/', 
+    base: mode === 'development' ? '/' : '/test/', 
     plugins: [vue()],
     resolve: {
       alias: {
